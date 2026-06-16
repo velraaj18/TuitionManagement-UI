248de94f998c43a5db1398622b8f2764226ecaa8
Velraaj <velraaj30@gmail.com>
2026-06-16 19:21:00 +0530

    Handled submit for login and started register component

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -2,18 +2,23 @@ import { Button } from "primereact/button";
 import { Card } from "primereact/card";
 import { InputText } from "primereact/inputtext";
 import { Password } from "primereact/password";
-import { useState } from "react";
+import { useState, type SubmitEventHandler } from "react";
+import { Link } from "react-router-dom";
 
 export function Login() {
-  const [Email, setEmail] = useState<string>("");
-  const [Passcode, setPasscode] = useState<string>("");
+  const [email, setEmail] = useState<string>("");
+  const [passcode, setPasscode] = useState<string>("");
 
-  console.log(Email, Passcode);
+  const handleSubmit : SubmitEventHandler<HTMLFormElement>= (e) => {
+    e.preventDefault();
+    console.log(email, passcode);
+  };
+
   return (
     <div>
       <div className="loginCard">
         <Card title="Login" className="flex justify-content-center">
-          <form action="Post">
+          <form onSubmit={handleSubmit}>
             <div className="flex flex-column gap-4">
               <InputText
                 placeholder="Email"
@@ -25,11 +30,13 @@ export function Login() {
                 onChange={(e) => setPasscode(e.target.value)}
               />
               <div className="flex justify-content-center">
-                <Button label="Submit" className="w-6" />
+                <Button label="Submit" className="w-6" type="submit"/>
               </div>
             </div>
           </form>
-          <p>Never Registered? <a href="/register">Click Here</a></p>
+          <p>
+            Never Registered? <Link to="/register">Click Here</Link>
+          </p>
         </Card>
       </div>
     </div>
--- src/pages/Register.tsx
+++ src/pages/Register.tsx
@@ -1,3 +1,26 @@
+import { Card } from "primereact/card";
+import { useState, type SubmitEventHandler } from "react";
+
 export const Register = () => {
-  return <>Register</>;
+  const [email, setEmail] = useState<string>("");
+  const [passcode, setPasscode] = useState<string>("");
+  const [firstName, setFirstName] = useState<string>("");
+  const [lastName, setlastName] = useState<string>("");
+
+  const handleSubmit: SubmitEventHandler<HTMLFormElement> = (e) => {
+    e.preventDefault();
+    console.log(email, passcode, firstName, lastName);
+  };
+  return (
+    <div>
+      <div className="registerCard">
+        <Card
+          title="Register"
+          pt={{ title: { className: "flex justify-content-center" } }}
+        >
+          <form onSubmit={handleSubmit}></form>
+        </Card>
+      </div>
+    </div>
+  );
 };
